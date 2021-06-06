b7e86d17cfc097c7a28165809040958c6fd90779
nataraj.marthi <nataraj.marthi@gmail.com>
2021-06-06 11:42:22 +0530

    manoharam

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
     <div className="App">
     <h1> Naveen with flowers!!!!</h1>
 	<h2> Vasanthi is sleeping now !!!!!</h2>
+	<h1> Susmitha is React Queen !!!!</h1>
+	<h4> Yamini is master of All technologies!!!</h4>
+	<h5> Vishnu priya is our telugu teacher !!!!</h5>
     </div>
   );
 }
