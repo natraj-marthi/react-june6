d19d0e0fc5e1a8b7f4362d76c39a41422dd23243
nataraj.marthi <nataraj.marthi@gmail.com>
2021-06-06 10:31:27 +0530

    react hosting

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    <h1> React Killers!!!!</h1>
+    </div>
+  );
+}
+export default App;
